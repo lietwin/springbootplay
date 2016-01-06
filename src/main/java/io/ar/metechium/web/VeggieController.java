aad35f6cd79e00681bd4771488c18f36287ec7c1
lietwin <ludi@a-r.io>
2016-01-06 17:09:14 +0100

    update controller with new views name

--- src/main/java/io/ar/metechium/web/VeggieController.java
+++ src/main/java/io/ar/metechium/web/VeggieController.java
@@ -2,6 +2,8 @@ package io.ar.metechium.web;
 
 import io.ar.metechium.domain.Vegetable;
 import io.ar.metechium.domain.VegetableRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,8 @@ import java.util.List;
 
 @Controller
 public class VeggieController {
+
+    public static final Logger logger = LoggerFactory.getLogger("VeggieController");
 
     @Autowired
     protected VegetableRepository veggieRepository;
@@ -30,23 +34,41 @@ public class VeggieController {
     public String listAll(Model model) {
         List<Vegetable> vegs = (List<Vegetable>) veggieRepository.findAll();
         model.addAttribute("vegetables", vegs);
-        return "veggieHello";
+        return "veggieList";
     }
 
-    @RequestMapping(value = "/veggie/add", method = RequestMethod.GET)
+    @RequestMapping(value = "/veggie/create", method = RequestMethod.GET)
     public String createOrUpdate(@RequestParam(value = "id", required = false) Long id, Model model){
         if(id != null){
          Vegetable veg  = veggieRepository.findOne(id);
+            logger.info(veg.toString());
             model.addAttribute("vegetable", veg);
         }else {
             model.addAttribute("vegetable", new Vegetable());
         }
-        return "veggieForm";
+
+        return "veggieUpdateForm";
     }
 
-    @RequestMapping(value = "/veggie/add", method = RequestMethod.POST)
-    public String createOrUpdateSave(@ModelAttribute Vegetable vegetable, Model model){
-        veggieRepository.save(vegetable);
+    //TODO update not working
+    @RequestMapping(value = "/veggie/create")
+    public String createOrUpdateSave(@RequestParam(value = "id", required = false) Long id, @ModelAttribute Vegetable vegetable, Model model) {
+        if (null != id) {
+            veggieRepository.findOne(id).setName(vegetable.getName());
+            veggieRepository.findOne(id).setPrice(vegetable.getPrice());
+            veggieRepository.findOne(id).setColor(vegetable.getColor());
+            veggieRepository.findOne(id).setQuantity(vegetable.getQuantity());
+            logger.info("Veggie update");
+        } else {
+            logger.info("Veggie Creation");
+            veggieRepository.save(vegetable);
+        }
+        return "redirect:/veggie";
+    }
+
+    @RequestMapping(value = "/veggie/delete", method = RequestMethod.GET)
+    public String delete(@RequestParam(value = "id", required = true) Long id, Model model) {
+        veggieRepository.delete(id);
         return "redirect:/veggie";
     }
 
