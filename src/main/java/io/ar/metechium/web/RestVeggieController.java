b1ba6066a3d9ce0670e595cb92aabc8f0f1e09d7
lietwin <ludi@a-r.io>
2016-01-11 12:22:21 +0100

    complete Rest CRUD support with the Update code and JSON "produces" and "consumes" annotations

--- src/main/java/io/ar/metechium/web/RestVeggieController.java
+++ src/main/java/io/ar/metechium/web/RestVeggieController.java
@@ -3,18 +3,17 @@ package io.ar.metechium.web;
 import io.ar.metechium.domain.Vegetable;
 import io.ar.metechium.domain.VegetableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/veggies")
+@RequestMapping(value = "/api/veggies", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class RestVeggieController {
     @Autowired
     private VegetableRepository vegetableRepository;
 
+    //Get all vegetable in stock
     @RequestMapping(value = "", method = RequestMethod.GET)
     public Iterable<Vegetable> getAllVegetables() {
         return vegetableRepository.findAll();
@@ -32,20 +31,23 @@ public class RestVeggieController {
         return vegetableRepository.findOne(id);
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public Vegetable addVegetable(Vegetable vegetable) {
+    @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Vegetable addVegetable(@RequestBody Vegetable vegetable) {
         return vegetableRepository.save(vegetable);
     }
 
-//    @RequestMapping(value="/edit/{id}", method = RequestMethod.POST)
-//    public Vegetable updateVegetable(@PathVariable Long id, @RequestBody Vegetable vegetable){
-//        return vegetableRepository.save(vegetable);
-//    }
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Vegetable updateVegetable(@PathVariable Long id, @RequestBody Vegetable vegetable) {
+        Vegetable update = vegetableRepository.findOne(id);
+        update.setPrice(vegetable.getPrice());
+        update.setQuantity(vegetable.getQuantity());
+        update.setName(vegetable.getName());
+        update.setColor(vegetable.getColor());
+        return vegetableRepository.save(update);
+    }
 
-
-    @RequestMapping(value = "/del/{id}", method = RequestMethod.GET)
-    public String deleteVegetable(@PathVariable Long id) {
+    @RequestMapping(value = "/del/{id}", method = RequestMethod.DELETE)
+    public void deleteVegetable(@PathVariable Long id) {
         vegetableRepository.delete(id);
-        return "Delete success";
     }
 }
