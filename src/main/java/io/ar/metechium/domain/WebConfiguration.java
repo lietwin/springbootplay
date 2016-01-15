6b9f809caf3caad35a0d867a08489d694fac5f10
Ludi AKUE <ludi@a-r.io>
2016-01-15 15:54:56 +0100

    Add basic security support in java generating simple login form with basic error handling

--- src/main/java/io/ar/metechium/domain/WebConfiguration.java
+++ src/main/java/io/ar/metechium/domain/WebConfiguration.java
@@ -1,10 +1,12 @@
 package io.ar.metechium.domain;
 
 import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
@@ -14,7 +16,8 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
  * translate the real request ip that is used by the users instead of the proxy IP
  */
 @Configuration
-class WebConfiguration extends WebMvcConfigurerAdapter {
+@EnableWebSecurity
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public RemoteIpFilter remoteIpFilter() {
         return new RemoteIpFilter();
@@ -25,9 +28,20 @@ class WebConfiguration extends WebMvcConfigurerAdapter {
         return new LocaleChangeInterceptor();
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
+    /*
+    the following will
+     - require auhentication to every URL in the application
+     - generate a login form
+     - allow the user with the sales user and sales password to authenticate
+     - allow the user to logout
+     */
+    @Autowired
+    public void defaultConfigure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("sales").password("sales").roles("USER");
     }
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+//    }
 }
